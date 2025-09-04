87dbcfbaf0af01f943563735571f31879d4a9dec
mka <zorin.mikhail.e@gmail.com>
2025-09-04 22:03:34 +0300

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+let cart = []
+let product = {
+    id: id
+}
+let addButton = document.getElementById('addButton')
+
+addButton.addEventListener("click", function(){
+    let cartList = document.createElement('div')
+})
